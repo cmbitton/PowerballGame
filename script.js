9b4c721fe5bbe330380e4ecad6d5f5483742eefe
Curtis Bitton <cmbitton@gmail.com>
2022-09-26 15:05:01 -0400

    Add styles and usernum functions
    Added styles.css and quick format of
    webpage layout. Wrote the functions to
    retrieve user numbers and check nums for
    duplicates and range.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+
+function getUserNums(){
+    const numlist = [...document.querySelectorAll(".usernumber")];
+    const userNums = [];
+    for(let i = 0; i < numlist.length; i++){
+        userNums.push(numlist[i].value);
+    }
+    return new Set(userNums);
+}
+function checkNums(numSet){
+    if (numSet.size < 5){
+        return false;
+    }
+    else{
+        for(const number of numSet){
+            if(number > 70 || number < 1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 function createLotteryNumberOptions() {
     const numberOptions = [];
     for (let i = 1; i <= 70; i++) {
