4f50e90d2747e3acd59923f3742fc79a28d95bf9
Curtis Bitton <cmbitton@gmail.com>
2022-09-26 18:20:55 -0400

    Add game functionality
    Added functions to create a bank
    and money system, buttons for playing
    singular games or until bank is empty,
    and functions that create a winning ticket
    and check matches against user ticket

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
+const errorScreen = document.querySelector('.info-screen');
+const playButton = document.querySelector('.play-button');
+const playMultipleButton = document.querySelector('.play-multiple-button');
+const winningNumOutput = document.querySelector('.winning-numbers');
+const bank = document.querySelector('.bank');
+let bankAmount = 0;
+bank.textContent = `Bank: $${bankAmount}`;
+//bank buttons:
+const bankButton = document.querySelector('.bank-button');
+bankButton.addEventListener('click', addMoneyToBank);
+
+function addMoneyToBank() {
+    const addedMoney = document.querySelector('.add-money');
+    if (addedMoney.value !== '') {
+        bankAmount += parseInt(addedMoney.value);
+        bank.textContent = `Bank: $${bankAmount}`;
+    }
+}
 
 
-function getUserNums(){
+function getUserNums() {
     const numlist = [...document.querySelectorAll(".usernumber")];
     const userNums = [];
-    for(let i = 0; i < numlist.length; i++){
+    for (let i = 0; i < numlist.length; i++) {
         userNums.push(numlist[i].value);
     }
     return new Set(userNums);
 }
-function checkNums(numSet){
-    if (numSet.size < 5){
+function checkNums(numSet) {
+    if (numSet.size < 5) {
         return false;
     }
-    else{
-        for(const number of numSet){
-            if(number > 70 || number < 1){
+    else {
+        for (const number of numSet) {
+            if (number > 70 || number < 1) {
                 return false;
             }
         }
@@ -30,11 +48,75 @@ function createLotteryNumberOptions() {
     return numberOptions;
 }
 
-function createWinningTicket(possiblenumbers) {
+function createWinningTicket(possibleNumbers) {
     const winningTicket = [];
     for (let i = 0; i < 5; i++) {
-        const winningNumber = possiblenumbers.splice([Math.floor(Math.random() * possiblenumbers.length)], 1)
+        const winningNumber = possibleNumbers.splice([Math.floor(Math.random() * possibleNumbers.length)], 1)
         winningTicket.push(winningNumber.toString());
     }
     return winningTicket;
 }
+
+function checkMatches(userNumbers, winningNumbers) {
+    const matchingNumbers = [];
+    for (const num of userNumbers) {
+        for (const winningnum of winningNumbers) {
+            if (num === winningnum) {
+                matchingNumbers.push(winningnum);
+            }
+        }
+    }
+    return matchingNumbers;
+}
+
+function playSingleGame() {
+    const userNums = getUserNums();
+    if (checkNums(userNums) === false) {
+        errorScreen.textContent = 'You must input 5 different numbers between 1 and 70'
+        winningNumOutput.textContent = '';
+    }
+    else {
+        if (bankAmount < 2) {
+            errorScreen.textContent = 'Not enough money in Bank';
+        }
+        else {
+            const lottoNumberList = createLotteryNumberOptions();
+            const winningNumbers = createWinningTicket(lottoNumberList);
+            const matchingNumbers = checkMatches(userNums, winningNumbers);
+            errorScreen.textContent = `Matching Numbers: ${matchingNumbers.join(' ')}`;
+            winningNumOutput.textContent = `Winning Numbers: ${winningNumbers.join(' ')}`;
+            bankAmount -= 2;
+        }
+    }
+}
+
+function playMultipleGames() {
+    const userNums = getUserNums();
+    if (checkNums(userNums) === false) {
+        errorScreen.textContent = 'You must input 5 different numbers between 1 and 70'
+        winningNumOutput.textContent = '';
+    }
+    else {
+        if (bankAmount < 2) {
+            errorScreen.textContent = 'Not enough money in Bank';
+        }
+        else {
+            for (bankAmount; bankAmount >= 2; bankAmount - 2) {
+                const lottoNumberList = createLotteryNumberOptions();
+                const winningNumbers = createWinningTicket(lottoNumberList);
+                const matchingNumbers = checkMatches(userNums, winningNumbers);
+                errorScreen.textContent = `Matching Numbers: ${matchingNumbers.join(' ')}`;
+                winningNumOutput.textContent = `Winning Numbers: ${winningNumbers.join(' ')}`;
+                bankAmount = bankAmount - 2;
+                bank.textContent = `Bank: ${bankAmount}`;
+            }
+        }
+    }
+}
+
+
+function startGame() {
+    playButton.addEventListener('click', playSingleGame)
+    playMultipleButton.addEventListener('click', playMultipleGames)
+}
+startGame();
